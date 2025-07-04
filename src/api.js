2985f55260b2c879a3c166967398723c9569d4ed
Gaurav Kumar <gaurav3108billa@gmail.com>
2025-07-04 13:45:08 +0530

    base url update

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const API = axios.create({
-  baseURL: 'http://localhost:8000', // adjust if needed
+  baseURL: 'https://aromacandles-backend.onrender.com', 
 });
 
 export default API;
